d0330782a61c8b5c677af6700c2d744c97555c5c
Victor <mahluzavictor@gmail.com>
2023-08-06 22:42:29 +0200

    fix:asgi channel routes

--- config/asgi.py
+++ config/asgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-from channels.routing import ProtocolTypeRouter
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
